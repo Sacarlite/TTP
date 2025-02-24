bc928f33ee14cc55eadd21a822b7c1b035f3a985
SacarLite <vladen.bokovoi@mail.ru>
2025-02-24 14:23:48 +0300

    Добавьте файлы проекта.

---
+++ TTP/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace TTP
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+        [Test]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+    }
+}
